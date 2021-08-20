c58bac00edf4cac56f08d8e09b6eac98ca089c8b
Valentin <valentyn.patsera@gmail.com>
2021-08-20 08:17:44 +0300

    Updates

--- precache-manifest.097823679098ee66b8973da922c5f220.js
+++ precache-manifest.097823679098ee66b8973da922c5f220.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "656a8efaf7e3d7b555b947b016bdb12e",
+    "revision": "39d8fbd61efd0e308a21a3959050a452",
     "url": "/user-o-matic-react/index.html"
   },
   {
@@ -8,24 +8,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/user-o-matic-react/static/css/3.1943ca39.chunk.css"
   },
   {
-    "revision": "952abc2938e460aa6e75",
+    "revision": "dff6d93efa6d2e225c9a",
     "url": "/user-o-matic-react/static/css/main.f76fbd91.chunk.css"
   },
   {
-    "revision": "2aa08a507f56cc2a1e76",
-    "url": "/user-o-matic-react/static/js/2.9ef5a3d9.chunk.js"
+    "revision": "87b37f4dbf955d57d899",
+    "url": "/user-o-matic-react/static/js/2.9dbb1818.chunk.js"
   },
   {
     "revision": "774152ec3dfb5dadc6880f2e7f79e400",
-    "url": "/user-o-matic-react/static/js/2.9ef5a3d9.chunk.js.LICENSE.txt"
+    "url": "/user-o-matic-react/static/js/2.9dbb1818.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0ed516bd21dda6e786ca",
     "url": "/user-o-matic-react/static/js/3.9d80f619.chunk.js"
   },
   {
-    "revision": "952abc2938e460aa6e75",
-    "url": "/user-o-matic-react/static/js/main.99df885e.chunk.js"
+    "revision": "dff6d93efa6d2e225c9a",
+    "url": "/user-o-matic-react/static/js/main.7bdb2e23.chunk.js"
   },
   {
     "revision": "5347da930d512ce004d3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/user-o-matic-react/precache-manifest.791fd3f6d0fe652992848ab1c1699665.js"
+  "/user-o-matic-react/precache-manifest.097823679098ee66b8973da922c5f220.js"
 );
 
 self.addEventListener('message', (event) => {
